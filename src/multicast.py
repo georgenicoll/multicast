41b07998e14dcfe5f3042128eb6549f1f9fd14b1
George Nicoll <george.nicoll@gmail.com>
2020-01-21 21:45:17 +0000

    Add Multicast script

---
+++ src/multicast.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#Shamelessly cribbed from https://github.com/dumplab/python_multicast/
+import sys
+import socket
+import struct
+import multiprocessing
+from time import sleep
+import time
+
+ADDRESS = "239.2.2.2"
+PORT = 31001
+TTL = 3
+READ = 'read'
+WRITE = 'write'
+PAYLOAD = 'Multicast Hello'
+ADDRESS_PORT = (ADDRESS, PORT)
+
+def main(argv):
+    print('Arguments: ', str(argv))
+
+    if len(argv) < 2:
+        print('Need a mode.  Expecting', READ, '|', WRITE)
+        exit(1)
+
+    mode = argv[1]
+    if mode == READ:
+        sockfunc=read
+    elif mode == WRITE:
+        sockfunc=write
+    else:
+        print('Unrecognised mode', mode, '. Expecting', READ, '|', WRITE)
+        exit(1)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sockfunc(sock)
+
+    print('Finished.')
+
+def read(sock):
+    print('Reading from', ADDRESS, PORT, '...')
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(ADDRESS_PORT)
+    mreq = struct.pack("4sl", socket.inet_aton(ADDRESS), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq) ## sending IGMPv2 membership queries
+    while True:
+        print(sock.recv(10240))
+        sleep(0.02)
+
+def write(sock):
+    print('Writing to', ADDRESS, PORT, '...')
+    while True:
+        print("Sending:", PAYLOAD)
+        sock.sendto(bytes(PAYLOAD,encoding='UTF-8'), ADDRESS_PORT)
+        sleep(1)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
